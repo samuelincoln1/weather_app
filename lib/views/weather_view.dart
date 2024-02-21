d91de03fcc62ecaa65e8576b2225c5d60ccf507e
samuelincoln1 <samuelincoln1@gmail.com>
2024-02-21 12:52:12 -0300

    basic config

---
+++ lib/views/weather_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WeatherView extends StatefulWidget {
+  const WeatherView({super.key});
+
+  @override
+  State<WeatherView> createState() => _WeatherViewState();
+}
+
+class _WeatherViewState extends State<WeatherView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
